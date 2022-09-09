106e0ca1137cf878a81d863a6a6b0afd524cc48f
Ketan Vaghela <ketanvaghela2230@gmail.com>
2022-09-09 12:00:42 +0530

    Add getData and deleteData Functionality in core

---
+++ src/controllers/core/request.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: "RequestCore",
+  data() {
+    return {
+      baseURL: "http://localhost:3000/",
+    };
+  },
+  methods: {
+    getRequest() {
+      return this;
+    },
+    getPost(key) {
+      return this;
+    },
+    getGet(key) {
+      return this;
+    },
+    getRequest(key) {
+      return this;
+    },
+    isGet() {
+      return true;
+    },
+    isPost() {
+      return true;
+    },
+    getRequestType() {
+      return type;
+    },
+  },
+};
+</script>
